2ee718002f0077c39e794a5dba0b5bf3f2b7d085
Spencer Lee <supensaa@gmail.com>
2019-05-23 15:27:07 -0700

    Update play_guessing_game.rb

--- play_guessing_game.rb
+++ play_guessing_game.rb
@@ -1,7 +1,4 @@
-# No need to change or write any code in this file.
-#
-# After you complete all specs, you can simulate your game by
-# running this file with `ruby lib/play_guessing_game.rb` in your terminal!
+# run this file with `ruby play_guessing_game.rb'
 
 require_relative "guessing_game"
 
